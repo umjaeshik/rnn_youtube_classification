59bb0fcc578b47c412774d17c03d646be841e727
pc1 <cute_js@daum.net>
2024-01-26 14:39:44 +0900

    job01 completed

--- job02_data_concat.py
+++ job02_data_concat.py
@@ -3,16 +3,15 @@ import glob
 import datetime
 
 last_data = []
-keyword = ['게임', 'GAME', '뉴스', 'NEWS', '음악', 'MUSIC', '요리', 'COOK', '애완동물', 'PETS', '스포츠', 'SPORTS']
-for i in keyword:
-    data_paths = glob.glob('./data/data_{}_*'.format(i))
-    if data_paths:
-        last_data.append(data_paths[-1])
+
+
+data_paths = glob.glob('./data/data_*')
+
 
 print(last_data)
 
 df = pd.DataFrame()
-for path in last_data:
+for path in data_paths:
     df_temp = pd.read_csv(path)
     df_temp.dropna(inplace=True)
     df = pd.concat([df, df_temp])
@@ -30,3 +29,4 @@ if not df.empty:
         datetime.datetime.now().strftime('%Y%m%d')), index=False)
 else:
     print("DataFrame is empty. No CSV file generated.")
+
