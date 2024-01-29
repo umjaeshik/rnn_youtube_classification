f9f864f5fb4801c407ccc174f36e1a951d1bd9ba
pc1 <cute_js@daum.net>
2024-01-29 16:49:43 +0900

    job01 completed

--- job00_predict_data_crawling.py
+++ job00_predict_data_crawling.py
@@ -2,13 +2,17 @@ from selenium import webdriver as wb
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.chrome.options import Options
+import random
+from selenium.webdriver.common.action_chains import ActionChains
+
+# 마우스를 menu 요소 중앙으로 이동한 뒤 hidden_submenu 요소를 클릭하는 것을 실행
+
 
 import re
 import random
 import time
 import pandas as pd
 import datetime
-
 
 def scroll():
     try:
@@ -40,50 +44,104 @@ def scroll():
         print("에러 발생: ", e)
 
 
+
+
+
 # f-string
 options = Options()
 key_count = 0
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+options.add_argument('User-Agent=' + user_agent)
+#options.add_argument('lang=ko_KR')
+
+
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument("disable-gpu")
+#
+ #driver = wb.Chrome(options=options)
 
 options.add_argument('--start-maximized')
 
 
 yt_url = 'https://www.youtube.com/'
 
-x_path = '//*[@id="chips"]/yt-chip-cloud-chip-renderer[*]'
-driver = wb.Chrome(options)
+
+driver = wb.Chrome(options=options)
 
 try:
     driver.get(yt_url)
 except:
     print('drivet.get', keyword)
 
-driver.find_element(By.XPATH, '//*[@id="chips"]/yt-chip-cloud-chip-renderer[{}]'.format(7)).click()
-time.sleep(2)
+for i in range(10):
 
+    text_list = driver.find_element(By.CLASS_NAME,'style-scope ytd-feed-filter-chip-bar-renderer').find_elements(By.CLASS_NAME,
+                                                                                                     'style-scope yt-chip-cloud-chip-renderer')
 
-scroll()
+    keywords = ['악','게임','축구', '야구', '농구','요리','음식','애완동물','반려동물','개','고양이','자연']
+    key_num = [0, 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6]
+    key_label = ['music','game','sports','cook','pets','nature']
+    class_list=[]
+    print(text_list)
+    for word in text_list:
+        class_list.append(word)
+    random.shuffle(class_list)
+    print(class_list)
+    #.find_elements(By.CLASS_NAME,'style-scope ytd-rich-grid-media')
+    for word in class_list:
+        if word.text in keywords:
+            label=key_label[key_num[keywords.index(word.text)]]
+            #ActionChains(driver).move_to_element(word).move_by_offset(random.uniform(-10,10),random.uniform(-5,5)).click().perform()
+            word.click()
+            time.sleep(random.uniform(2,3))
 
+            scroll()
 
-html = bs(driver.page_source, 'html.parser')
-#print(html)
+            dr_title =driver.find_elements(By.ID,'video-title-link')
+            titleList =[]
+            for list in dr_title:
+                title = list.find_element(By.ID,'video-title').text
+                title = re.compile('[^가-힣a-zA-Z]').sub(' ', title)
+                titleList.append(title)
+            print(titleList)
+            now=datetime.datetime.now()
+            df_titles = pd.DataFrame()
+            df_section_title = pd.DataFrame(titleList, columns=['titles'])
+            df_section_title['category'] = label
+            df_titles = pd.concat([df_titles, df_section_title], axis='rows', ignore_index=True)
+            print(df_titles.head())
+            df_titles.to_csv('./predict_data/data_{}_{}_{}.csv'.format(label, now.strftime('%d'),i),index=False)
+            titleList = []
 
+    driver.find_element(By.XPATH,'/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[1]/ytd-topbar-logo-renderer/a/div/ytd-logo/yt-icon/yt-icon-shape/icon-shape/div').click()
+    time.sleep(5)
 driver.close()
+        #     # # print(html)
+    #     #
+        # driver.close()
+        #
+        # titleList = []
+        # dr_title =driver.find_element(By.ID,'video-title').find_elements(By.CLASS_NAME,'style-scope ytd-rich-grid-media')
+        # for content in dr_title:
+        #     title = content.text
+        #     title = re.compile('[^가-힣a-zA-Z]').sub(' ', title)
+        #
+        #     titleList.append(title)
+        #
+        # df_titles = pd.DataFrame()
+        # df_section_title = pd.DataFrame(titleList, columns=['titles'])
+        # df_section_title['category'] = label
+        # df_titles = pd.concat([df_titles, df_section_title], axis='rows', ignore_index=True)
+        # df_titles.to_csv('./predict_data/data_{}_{}.csv'.format(label, datetime.datetime.now().strftime('%Y%m%d')),
+        #                  index=False)
+        #
+        # # print(label)
 
-titleList=[]
-
-for content in html.select('a#video-title-link'):
-    title = content.get('title')
-    title = re.compile('[^가-힣a-zA-Z]').sub(' ', title)
-
-
-    titleList.append(title)
-
-df_titles = pd.DataFrame()
-df_section_title = pd.DataFrame(titleList, columns=['titles'])
-df_section_title['category'] = 'game'
-df_titles = pd.concat([df_titles, df_section_title], axis='rows', ignore_index=True)
-df_titles.to_csv('./predict_data/data_{}_{}.csv'.format('game', datetime.datetime.now().strftime('%Y%m%d')), index=False)
 
 
 
 
+
+#
+#
